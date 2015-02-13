d4407bc09bfb70643804df32e036320507189385
Gillinghammer <colingillingham@gmail.com>
2015-02-12 23:59:26 -0800

    colorscore

--- colorSync.rb
+++ colorSync.rb
@@ -1,5 +1,5 @@
 require 'av_capture'
-require 'rmagick'
+require 'Colorscore'
 require 'color'
 require 'lifx'
 
@@ -13,28 +13,20 @@ dev     = AVCapture.devices.find(&:video?)
 p dev.name
 p dev.video?
 
-100.times do |run|
+  100.times do |run|
 
-  session.run_with(dev) do |connection|
-    5.times do |i|
-      File.open("x_#{i}.jpg", 'wb') { |f|
-        f.write connection.capture
-      }
-      sleep 1
+    session.run_with(dev) do |connection|
+        File.open("x_1.jpg", 'wb') { |f| f.write connection.capture }
+        histogram =  Colorscore::Histogram.new('x_1.jpg').first
+        puts "WTF"
+        puts histogram
+        red = histogram.scores.first.last.red
+        green = histogram.scores.first.last.green
+        blue = histogram.scores.first.last.blue
+
+        screen_capture_color = LIFX::Color.rgb(red, green, blue)
+      light.set_color(screen_capture_color, duration: 1)  # Light#set_color is asynchronous
+      sleep 1                                    # Wait for light to finish changing
     end
-  end
-
-  img =  Magick::Image.read('x_4.jpg').first
-  pix = img.scale(1, 1)
-  # averageColor = pix.pixel_color(0,0)
-  avg_color_hex = pix.to_color(pix.pixel_color(0,0))
-
-  puts avg_color_hex;
-
-  c = Color::RGB.from_html(avg_color_hex[1..-1])
-
-  screen_capture_color = LIFX::Color.rgb(c.red,c.green,c.blue)
-  light.set_color(screen_capture_color, duration: 1)  # Light#set_color is asynchronous
-  sleep 1                                    # Wait for light to finish changing
 
 end
